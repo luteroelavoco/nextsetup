2b333cd98d991c2a5cc61958e70b60405e8afbef
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-12-28 23:37:16 -0300

    feat: eslint and prettier

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+
+  return (
+    <div>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  )
+}
+export default Home
