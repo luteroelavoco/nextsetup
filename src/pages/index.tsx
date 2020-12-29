902bbda161e518cb3cbb0302a14004779d98286a
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-12-29 01:18:32 -0300

    feat: react images plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Head from 'next/head'
-
+import {Container} from '../styles/pages/Home';
 const Home: React.FC = () => {
 
   return (
-    <div>
+    <Container>
       <Head>
         <title>Home Page</title>
       </Head>
 
       <main>
-        <h1>Hello World</h1>
+        <h1>Brevemente Disponível</h1>
+        <p>@Copyright 2020 Code Pro Inc. All rights reserved.</p>
       </main>
-    </div>
+    </Container>
   )
 }
 export default Home
