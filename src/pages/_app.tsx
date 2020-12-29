1ef30740a0e39d2824ce4a3d80f918141d699feb
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-12-29 00:57:14 -0300

    feat: styled components config on documents

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-import {AppProps} from 'next/app'
+import { AppProps } from 'next/app'
+import GlobalStyle from '../styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from '../styles/theme'
 
-const MyApp: React.FC<AppProps>  = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -28,5 +35,23 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+  render() :JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
